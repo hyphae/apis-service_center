d83e52b3e688159160b4ca35654df7f2517af51c
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-11-26 17:10:19 +0900

    modified resident.py

--- scenario/urls/resident.py
+++ scenario/urls/resident.py
@@ -3,7 +3,7 @@ from scenario import views
 
 app_name = 'scenario'
 urlpatterns = [
-	path('available_list', views.resident_available_list, name = 'available_list'),
+	path('availableList', views.resident_available_list, name = 'available_list'),
 	path('current', views.resident_current, name = 'current'),
 	path('choose', views.resident_choose, name = 'choose'),
 ]
